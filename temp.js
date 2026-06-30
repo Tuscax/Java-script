26161993e5cb94733357c80d8753592f2a5a318d
JoseAlencar0922 <joselucas3011@gmail.com>
2026-06-30 19:45:27 -0300

    oii

--- temp.js
+++ temp.js
@@ -1,26 +1,26 @@
-function celc() {
-    const um = confirm("Você quer converter um celcsius para Fahrenheit?") 
-    if (um === true) {
-        const dois = Number(prompt("digite um número"))
-        let tres = (dois*9/5) + 32 
-        alert(tres)
-        return
-    } else {
-        return
+// function celc() {
+//     const um = confirm("Você quer converter um celcsius para Fahrenheit?") 
+//     if (um === true) {
+//         const dois = Number(prompt("digite um número"))
+//         let tres = (dois*9/5) + 32 
+//         alert(tres)
+//         return
+//     } else {
+//         return
         
-    }
-}
-celc()
-function fare() {
-    const um = confirm("Você quer converter um Fahrenheit para  celcsius?") 
-    if (um === true) {
-        const dois = Number(prompt("digite um número"))
-        let tres = (dois*9/5) + 32 
-        console.log(tres)
+//     }
+// }
+// celc()
+// function fare() {
+//     const um = confirm("Você quer converter um Fahrenheit para  celcsius?") 
+//     if (um === true) {
+//         const dois = Number(prompt("digite um número"))
+//         let tres = (dois*9/5) + 32 
+//         console.log(tres)
         
-    } else {
-        return
+//     } else {
+//         return
         
-    }
-}
-fare()
+//     }
+// }
+// fare()
